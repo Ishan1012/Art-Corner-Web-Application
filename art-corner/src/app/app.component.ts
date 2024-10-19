6f8764249ac2d92a5a426d7d11372d9884fec833
Ishan Dwivedi <ishandwivedi1210@gmail.com>
2024-10-19 17:47:18 +0530

    Major Updates in Explore page 1.3.0

--- art-corner/src/app/app.component.ts
+++ art-corner/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ExploreComponentsComponent } from "./art-gallery/explore-components/explore-components.component";
 import { IndexPageComponent } from "./art-gallery/index-page/index-page.component";
+import { FeedbackAboutUsComponent } from "./feedback-about-us/feedback-about-us.component";
+import { AccessibilityFeaturesComponent } from "./accessibility-features/accessibility-features.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ExploreComponentsComponent, IndexPageComponent],
+  imports: [RouterOutlet, ExploreComponentsComponent, IndexPageComponent, FeedbackAboutUsComponent, AccessibilityFeaturesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
