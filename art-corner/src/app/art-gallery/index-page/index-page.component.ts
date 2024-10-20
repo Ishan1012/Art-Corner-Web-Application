41110d2b7383559873a57b71281ac6c69f683e1d
Ishan Dwivedi <ishandwivedi1210@gmail.com>
2024-10-20 20:02:44 +0530

    Added Index Page 2.5.2

--- art-corner/src/app/art-gallery/index-page/index-page.component.ts
+++ art-corner/src/app/art-gallery/index-page/index-page.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { PagesContainer } from '../../PagesContainer';
 
 @Component({
   selector: 'app-index-page',
   standalone: true,
   imports: [],
   templateUrl: './index-page.component.html',
-  styleUrl: './index-page.component.css'
+  styleUrls: ['./index-page.component.css'] // Fixed from 'styleUrl' to 'styleUrls'
 })
-export class IndexPageComponent {
+export class IndexPageComponent{
+  @Input() pages: PagesContainer = new PagesContainer;
+  OpenExplore() {
+    this.pages.pageNo = 2;
+  }
 }
--- art-corner/src/app/educational-resources/educational-resources.component.ts
+++ art-corner/src/app/educational-resources/educational-resources.component.ts
@@ -1,16 +1,43 @@
-import { Component, Input } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
 import { CreateExplore } from '../art-gallery/explore-components/CreateExplore';
-import { NgIf } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
+import { initialArtifacts } from '../data/initialArtifacts';
 
 @Component({
   selector: 'app-educational-resources',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf,NgClass],
   templateUrl: './educational-resources.component.html',
   styleUrl: './educational-resources.component.css'
 })
 export class EducationalResourcesComponent {
   @Input() currentItem: CreateExplore = new CreateExplore;
+  @Input() artifacts: CreateExplore[];
+
+  constructor(){
+    this.artifacts = initialArtifacts;
+  }
   
   item: CreateExplore = this.currentItem;
+
+  toggleLike(item: CreateExplore) {
+    const index = this.artifacts.indexOf(item);
+    if (index !== -1) {
+      this.artifacts[index].like = !this.artifacts[index].like;
+    }
+  }
+  
+  moveBack(item: CreateExplore) {
+    const index = this.artifacts.indexOf(item);
+    if (index-1 !== -1) {
+      this.currentItem = this.artifacts[index-1];
+    }
+  }
+
+  moveNext(item: CreateExplore) {
+    const index = this.artifacts.indexOf(item);
+    if (index+1 !== this.artifacts.length) {
+      this.currentItem = this.artifacts[index+1];
+    }
+  }
 }
